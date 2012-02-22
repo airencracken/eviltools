8ba0a36fa8682e89bd93ea12553a69032c5be23d
Marcus Hildum <mjhildum@csupomona.edu>
2012-02-22 15:41:28 -0800

    first commit

---
+++ nasty.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use warnings;
+#use strict;
+#$SIG{'INT'} = 'IGNORE';
+#$SIG{'STOP'} = 'IGNORE';
+#$SIG{'KILL'} = 'IGNORE';
+my $sharg = $ARGV[0];
+print $sharg;
+my $please = "please";
+my $i = 0;
+while ($sharg ne $please && $i<3){
+    print "access denied\n";
+    $i++;
+    $sharg = <>;
+    chomp $sharg;
+}
+if($i>=3){
+  while (1){
+   print "you didn't say the magic word\n";
+   #fork;
+  }
+} else {
+  exec "/bin/su";
+}
+
